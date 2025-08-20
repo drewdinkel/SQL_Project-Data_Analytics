b1ae14af41335949ccc1f09180e560f71dd13bf7
drewdinkel <drewdinkel3417@gmail.com>
2025-08-19 20:35:52 -0500

    first commit

---
+++ Learning/3_cases.sql
@@ -0,0 +1,22 @@
+/*
+
+Label new column as follows:
+- 'Anywhere' jobs as 'Remote'
+- 'New York, NY' as 'Local'
+- Other 'Onsite'
+
+*/
+
+SELECT
+    COUNT(job_id) AS number_of_jobs,
+    CASE
+        WHEN job_location = 'Anywhere' THEN 'Remote'
+        WHEN job_location = 'New York, NY' THEN 'Local'
+        ELSE 'Onsite'
+    END AS location_category
+FROM
+    job_postings_fact
+WHERE
+    job_title_short = 'Data Analyst'
+GROUP BY
+    location_category;
